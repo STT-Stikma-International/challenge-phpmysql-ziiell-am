a8d121fecfa4e0053b7018b033d21d38cea8f7b6
fazriel <fazriel.keysha@gmail.com>
2026-04-24 15:24:05 +0700

    24112693

--- challenge.php
+++ challenge.php
@@ -8,15 +8,18 @@ $email = $_POST['email'];
 
 
 //TODO: insert nama, nim, kelas, email
+// $sql = "INSERT INTO mahasiswa (nama, nim, kelas, email)
+// VALUES
+// ('$nama', '$nim', '$kelas', '$email')";
 
-// ---
-if ($conn->query($sql) === TRUE) {
-    echo "Data berhasil disimpan";
-} else {
-    echo "Error: " . $conn->error;
-}
+// // ---
+// if ($conn->query($sql) === TRUE) {
+//     echo "Data berhasil disimpan";
+// } else {
+//     echo "Error: " . $conn->error;
+// }
 
-$conn->close();
+// $conn->close();
 
 
 
@@ -42,35 +45,36 @@ $conn->close();
 
 
 //TODO: SELECT data mahasiswa
+// $sql = "SELECT nama, nim, kelas, email FROM mahasiswa";
+// $result = $conn->query($sql);
+// // ---
+// if ($result->num_rows > 0) {
 
-// ---
-if ($result->num_rows > 0) {
+//     echo "<table border='1'>";
+//     echo "<tr>
+//             <th>ID</th>
+//             <th>Nama</th>
+//             <th>NIM</th>
+//             <th>Kelas</th>
+//             <th>Email</th>
+//         </tr>";
 
-    echo "<table border='1'>";
-    echo "<tr>
-            <th>ID</th>
-            <th>Nama</th>
-            <th>NIM</th>
-            <th>Kelas</th>
-            <th>Email</th>
-        </tr>";
+//     while($row = $result->fetch_assoc()) {
+//         echo "<tr>
+//                 <td>".$row['id']."</td>
+//                 <td>".$row['nama']."</td>
+//                 <td>".$row['nim']."</td>
+//                 <td>".$row['kelas']."</td>
+//                 <td>".$row['email']."</td>
+//             </tr>";
+//     }
 
-    while($row = $result->fetch_assoc()) {
-        echo "<tr>
-                <td>".$row['id']."</td>
-                <td>".$row['nama']."</td>
-                <td>".$row['nim']."</td>
-                <td>".$row['kelas']."</td>
-                <td>".$row['email']."</td>
-            </tr>";
-    }
+//     echo "</table>";
 
-    echo "</table>";
-
-} else {
-    echo "Data tidak ditemukan";
-}
-$conn->close();
+// } else {
+//     echo "Data tidak ditemukan";
+// }
+// $conn->close();
 
 
 
@@ -95,7 +99,8 @@ $conn->close();
 
 
 //TODO: UPDATE DATA MAHASISWA
-
+$sql =  "UPDATE mahasiswa
+SET email = 'aefil@mail.com' where id = 4";
 //---
 if ($conn->query($sql) === TRUE) {
     echo "Data berhasil diupdate";
